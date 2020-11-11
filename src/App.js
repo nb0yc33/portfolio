d9102ee64a48dcde312040a039408dfc948721a5
Nicholas Boyce <32978779+nb0yc33@users.noreply.github.com>
2020-11-11 19:13:27 +1000

    Home page changes

--- src/App.js
+++ src/App.js
@@ -8,28 +8,29 @@ import Home from './home.jsx';
 import './App.scss';
  
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="navigation">
-          <img src={logo} className="logo-image" alt="Nicholas Boyce Logo" />
-          <div className="navigation-sub">
-                                          
-            <Link to="/projects" className="item" name="projects">Projects</Link>
-            <Link to="/strengths" className="item" name="strengths">Strengths</Link>
-            <Link to="/about" className="item" name="about">About</Link>
-  
-          </div>
-        </div>
-
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/strengths" component={Strengths} />
-          <Route path="/about" component={About} />
-          <Route exact path="/" component={Home} />
-      </div>
-    </BrowserRouter>
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <div className="navigation">
+            <img src={logo} className="logo-image" alt="Nicholas Boyce Logo" />
+            <div className="navigation-sub">
+                                            
+              <Link to="/projects" className="item" name="projects">Projects</Link>
+              <Link to="/strengths" className="item" name="strengths">Strengths</Link>
+              <Link to="/about" className="item" name="about">About</Link>
     
-  );
+            </div>
+          </div>
+
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/strengths" component={Strengths} />
+            <Route path="/about" component={About} />
+            <Route path="/portfolio" component={Home} />
+        </div>
+      </BrowserRouter>
+
+    );
+
 }
  
 export default App;
